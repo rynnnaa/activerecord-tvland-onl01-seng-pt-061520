8d323bc6ee3e4023e341bcdaed8644454b480d68
Ryna Truong <rynatruong@outlook.com>
2020-08-31 17:47:32 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
   
+  def actors_list
+    self.actors.map do |actor|
+      actor.full_name
+    end
+  end
+    
+  def build_network(call_letters)
+    self.network = Network.new(call_letters)
+  end
 end
