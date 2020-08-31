97c1b1d202655ee8d4926ec503ff4a3d0347359d
Ryna Truong <rynatruong@outlook.com>
2020-08-31 17:44:42 +0000

    Automatically backed up by Learn

--- db/migrate/20200831174331_add_a_day.rb
+++ db/migrate/20200831174331_add_a_day.rb
@@ -1,6 +1,8 @@
 class AddADay < ActiveRecord::Migration[5.2]
   def change
     add_column :network :day :string
+    add_column :network :genre :string
+    add_column :network :season :string
     
     
   end
